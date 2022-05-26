604540cc9c373d711ec4412cac4b0ce0ba4352c8
Aminu Ismail <63140904+ibnabbas1429@users.noreply.github.com>
2022-05-26 17:36:20 +0000

    a simple anagram script

---
+++ main.py
@@ -0,0 +1,17 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    
+    are_words_anagrams = True
+    
+    if (sorted(word) == sorted(anagram)):
+        return are_words_anagrams
+    else:
+        return(False)
+
+    
+    
